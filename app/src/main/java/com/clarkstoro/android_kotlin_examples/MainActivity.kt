8f46739737484e07b5b5453050fca83c70ed8147
ClarkStoro <emanuele.maso97@gmail.com>
2019-02-13 17:06:42 +0100

    Navigation Drawer

---
+++ app/src/main/java/com/clarkstoro/android_kotlin_examples/MainActivity.kt
@@ -0,0 +1,126 @@
+package com.clarkstoro.android_kotlin_examples
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.core.view.GravityCompat
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import android.R.attr.fragment
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import androidx.fragment.app.FragmentManager
+import kotlinx.android.synthetic.main.content_main.*
+import java.lang.ClassCastException
+
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+
+    var firstTime = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
+
+        selectFragment(HomeFragment())
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+
+
+
+    //Create fragment selected for showing
+    private fun selectFragment(frgClass: Fragment){
+        var fragment: Fragment? = null
+        val fragmentClass =  frgClass::class.java
+        try{
+            fragment = fragmentClass.newInstance() as Fragment
+        }catch (e: ClassCastException){
+            e.printStackTrace()
+        }
+
+        replaceFragment(fragment as Fragment)
+    }
+
+    //Show the fragment selected
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+
+        if(fragment::class == HomeFragment::class){
+            if(firstTime){ //the first time the system opens Home Fragment is onCreate
+                firstTime = false
+            }else{
+                supportFragmentManager.popBackStack() //pop last fragment
+            }
+            fragmentTransaction.replace(R.id.content_main, fragment)
+        }else {
+            //When user click on back button return back to HomeFragment before quitting
+            supportFragmentManager.popBackStack() //pop last fragment
+            fragmentTransaction.replace(R.id.content_main, fragment).addToBackStack(null)//add to stack the new fragment
+        }
+        fragmentTransaction.commit()
+    }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+       when (item.itemId) {
+            R.id.nav_home -> {
+                selectFragment(HomeFragment())
+            }
+            R.id.nav_array_list -> {
+                selectFragment(ArrayListFragment())
+            }
+            R.id.nav_json_array_list ->{
+                selectFragment(JSONArrayListFragment())
+            }
+
+            else ->{
+                null
+           }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
